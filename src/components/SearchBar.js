ac696c22f99067355cb114d8d6baf433bc1d6e30
Patrick Bowes <patrick@patricks-mbp.myfiosgateway.com>
2022-06-10 19:04:23 -0400

    display data onto page

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+
 function SearchBar(props) {
     let [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form>
-            <input type="text" placeholder="Enter a search term here" />
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+            <input
+                type="text"
+                placeholder="Search Here"
+                onChange={(e) => props.handleSearch(e, e.target.value)} />
+
             <input type="submit" />
         </form>
     )
